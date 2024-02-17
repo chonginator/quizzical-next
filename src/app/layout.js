bbeb34e9f755491bad3f1522f62da71b73524ce9
Ben Chong <benchong1999@yahoo.com.au>
2024-02-17 23:13:34 +1000

    Add global styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,8 @@ import clsx from "clsx";
 import { primaryFont, secondaryFont } from "./fonts";
 import { COLOURS } from "@/constants";
 
+import styles from "./globals.css";
+
 export const metadata = {
   title: "Quizzical",
   description: "Let's get quizzical",
